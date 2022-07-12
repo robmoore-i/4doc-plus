af89381d01bcc92b8ec013511519cc4d260208df
Rob Moore <rmoore@gradle.com>
2022-07-12 22:41:00 +0800

    Extract method for doing "by shell"

--- gbt/build.gradle.kts
+++ gbt/build.gradle.kts
@@ -11,6 +11,15 @@ fun TaskContainer.shell(name: String, cmd: List<String>): TaskProvider<Task> =
         shell(cmd)
     }
 
+fun <T : Task, C : NamedDomainObjectContainer<T>> C.shell(
+    cmd: List<String>,
+    action: T.() -> Unit = {}
+): RegisteringDomainObjectDelegateProviderWithAction<out C, T> =
+    RegisteringDomainObjectDelegateProviderWithAction.of(this) {
+        shell(cmd)
+        action(this)
+    }
+
 object DockerApp {
     const val imageName = "gbt"
     const val containerName = "gbt-local"
@@ -19,18 +28,14 @@ object DockerApp {
 tasks {
     shell("mkdocsServe", listOf("mkdocs", "serve"))
 
-    val mkdocsBuild by registering {
-        shell(listOf("mkdocs", "build"))
-    }
+    val mkdocsBuild by shell(listOf("mkdocs", "build"))
 
-    val dockerBuild by registering {
+    val dockerBuild by shell(listOf("docker", "build", ".", "-t", DockerApp.imageName)) {
         dependsOn(mkdocsBuild)
-        shell(listOf("docker", "build", ".", "-t", DockerApp.imageName))
     }
 
-    val dockerRun by registering {
+    val dockerRun by shell(listOf("docker", "run", "-d", "-p", "8080:8080", "--name", DockerApp.containerName, DockerApp.imageName)) {
         dependsOn(dockerBuild)
-        shell(listOf("docker", "run", "-d", "-p", "8080:8080", "--name", DockerApp.containerName, DockerApp.imageName))
     }
 
     shell("dockerDown", listOf("docker", "rm", "-f", DockerApp.containerName))
