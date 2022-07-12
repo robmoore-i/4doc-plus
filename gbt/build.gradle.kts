85661cadaee6a915ca5d9653ff22cc90fbbf83dd
Rob Moore <rmoore@gradle.com>
2022-07-12 17:07:46 +0800

    Use tasks block

--- gbt/build.gradle.kts
+++ gbt/build.gradle.kts
@@ -1,39 +1,41 @@
 fun Task.shell(cmd: List<String>) {
-  doFirst {
-    exec {
-      commandLine(cmd)
+    doFirst {
+        exec {
+            commandLine(cmd)
+        }
     }
-  }
 }
 
-tasks.register("mkdocsServe") {
-  shell(listOf("mkdocs", "serve"))
-}
+tasks {
+    register("mkdocsServe") {
+        shell(listOf("mkdocs", "serve"))
+    }
 
-val mkdocsBuild by tasks.registering {
-  shell(listOf("mkdocs", "build"))
-}
+    val mkdocsBuild by registering {
+        shell(listOf("mkdocs", "build"))
+    }
 
-val dockerBuild by tasks.registering {
-  mustRunAfter(mkdocsBuild)
-  shell(listOf("docker", "build", ".", "-t", "gradle-4doc"))
-}
+    val dockerBuild by registering {
+        mustRunAfter(mkdocsBuild)
+        shell(listOf("docker", "build", ".", "-t", "gradle-4doc"))
+    }
 
-val containerName = "gradle-4doc-plus"
+    val containerName = "gradle-4doc-plus"
 
-val dockerRun by tasks.registering {
-  mustRunAfter(dockerBuild)
-  shell(listOf("docker", "run", "-d", "-p", "8080:8080", "--name", containerName, "gradle-4doc"))
-}
+    val dockerRun by registering {
+        mustRunAfter(dockerBuild)
+        shell(listOf("docker", "run", "-d", "-p", "8080:8080", "--name", containerName, "gradle-4doc"))
+    }
 
-tasks.register("dockerDown") {
-  shell(listOf("docker", "rm", "-f", containerName))
-}
+    register("dockerDown") {
+        shell(listOf("docker", "rm", "-f", containerName))
+    }
 
-tasks.register("dockerUp") {
-  dependsOn(mkdocsBuild, dockerBuild, dockerRun)
-}
+    register("dockerUp") {
+        dependsOn(mkdocsBuild, dockerBuild, dockerRun)
+    }
 
-tasks.register("flyDeploy") {
-  shell(listOf("fly", "deploy"))
+    register("flyDeploy") {
+        shell(listOf("fly", "deploy"))
+    }
 }
