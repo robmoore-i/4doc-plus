b8a12b82b38e1c0f35ccfd95f7416146ec4c0f05
Rob Moore <rmoore@gradle.com>
2022-07-16 16:06:21 +0800

    First step of federating docs in one image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,35 +6,51 @@ plugins {
 }
 
 dockerApps {
-    register("gbt")
-    register("ge")
+    register("docs")
 }
 
 flyApps {
-    register("gbt")
+    // TODO: Create this app
+    register("docs")
 }
 
-dockerApps.all {
-    val mkdocsConfiguration = configurations.create("mkdocsConfiguration${name.capitalize()}") {
+val syncMkdocsSourcesTasks = listOf("gbt", "ge").map { projectName ->
+    val mkdocsConfiguration = configurations.create("mkdocsConfiguration${projectName.capitalize()}") {
         isCanBeConsumed = false
         isCanBeResolved = true
         attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(name))
     }
 
-    val mkdocsDocs = configurations.create("mkdocsDocs${name.capitalize()}") {
+    val mkdocsDocs = configurations.create("mkdocsDocs${projectName.capitalize()}") {
         isCanBeConsumed = false
         isCanBeResolved = true
         attributes.attribute(Usage.USAGE_ATTRIBUTE, objects.named(name))
     }
 
     dependencies {
-        add(mkdocsConfiguration.name, project(":$name"))
-        add(mkdocsDocs.name, project(":$name"))
+        add(mkdocsConfiguration.name, project(":$projectName"))
+        add(mkdocsDocs.name, project(":$projectName"))
     }
 
-    tasks.named<DockerBuild>("dockerBuild${imageName.get().capitalize()}") {
-        resources.from(layout.projectDirectory.file("nginx.conf"))
-        resources.from(mkdocsConfiguration)
-        resources.from(mkdocsDocs)
+    val mkdocsSources = layout.buildDirectory.dir("mkdocsSources").map { it.dir("${projectName}-mkdocs") }
+
+    val syncMkdocsConfiguration = tasks.register<Sync>("syncMkdocsConfiguration${projectName.capitalize()}") {
+        from(mkdocsConfiguration)
+        into(mkdocsSources)
     }
+
+    val syncMkdocsDocs = tasks.register<Sync>("syncMkdocsDocs${projectName.capitalize()}") {
+        from(mkdocsDocs)
+        into(mkdocsSources.map { it.dir("docs") })
+    }
+
+    tasks.register("syncMkdocsSources${projectName.capitalize()}") {
+        dependsOn(syncMkdocsConfiguration, syncMkdocsDocs)
+        outputs.dir(syncMkdocsConfiguration.map { it.destinationDir })
+    }
+}
+
+tasks.named<DockerBuild>("dockerBuildDocs") {
+    resources.from(layout.projectDirectory.file("nginx.conf"))
+    syncMkdocsSourcesTasks.forEach { resources.from(it) }
 }
