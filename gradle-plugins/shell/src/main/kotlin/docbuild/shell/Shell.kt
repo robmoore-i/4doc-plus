0a5f3f94eb90e3ca95f5240a019b730bb2b0fa50
Rob Moore <rmoore@gradle.com>
2022-08-03 18:16:03 +0800

    Upgrade to Gradle 7.5

--- gradle-plugins/shell/src/main/kotlin/docbuild/shell/Shell.kt
+++ gradle-plugins/shell/src/main/kotlin/docbuild/shell/Shell.kt
@@ -4,18 +4,19 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecOperations
 import org.gradle.work.DisableCachingByDefault
 import javax.inject.Inject
 
 @DisableCachingByDefault
-abstract class Shell @Inject constructor() : DefaultTask() {
+abstract class Shell @Inject constructor(private val exec: ExecOperations) : DefaultTask() {
 
     @get:Input
     abstract val cmd: ListProperty<String>
 
     @TaskAction
     fun run() {
-        project.exec {
+        exec.exec {
             commandLine(cmd.get())
         }
     }
